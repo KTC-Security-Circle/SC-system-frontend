3e7ec0ecfae1b03592cf1d372595a96a48c8bc44
hourensou26 <ktc23a31g0002@edu.kyoto-tech.ac.jp>
2024-10-30 09:35:10 +0900

    Submissionpageの追加

---
+++ src/app/Submission/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { SubmissionComponents } from '../../Components/SubmissionPage';;
+
+const SubmissionPage:React.FC = () => {
+    return (
+        <SubmissionComponents />
+    )
+}
+
+export default SubmissionPage;
