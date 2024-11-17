1604d252f0ca871404e59d3aa3568de6c2b6b56b
Kitagawa19 <kkitagawa16219@gmail.com>
2024-11-17 16:13:25 +0900

    add 認証を必要とするページで確認を行うためのミドルウェアを作成

--- src/middleware/middleware.ts
+++ src/middleware/middleware.ts
@@ -2,13 +2,17 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
+  if (request.method === 'OPTIONS') {
+    return NextResponse.next();
+  }
   const token = request.cookies.get('access_token');
   if (!token) {
-    return NextResponse.redirect(new URL('/', request.url));
+    return NextResponse.redirect(new URL('http://localhost:3000/', request.url));
   }
   return NextResponse.next();
 }
 
 export const config = {
   matcher: ['/Chat*'], 
+  exclude: ['/'],
 };
