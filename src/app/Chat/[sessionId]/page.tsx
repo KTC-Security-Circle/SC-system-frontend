f5e892a6fcbad40e3625fc8c466f8e8341fe5872
Kitagawa19 <kkitagawa16219@gmail.com>
2024-12-20 22:25:42 +0900

    create 動的ページの作成(#19)

---
+++ src/app/Chat/[sessionId]/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { DynamicChat } from '@/Components/parts/Chat/DynamincChat';
+const ChatPage: React.FC = () => {
+  return (
+    <DynamicChat />
+  );
+};
+export default ChatPage;
