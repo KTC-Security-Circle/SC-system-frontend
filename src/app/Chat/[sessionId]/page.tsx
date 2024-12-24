0a5ceba5a7166c3d295696ad796c1880ca8fdee1
Kitagawa19 <kkitagawa16219@gmail.com>
2024-12-24 23:18:44 +0900

    fix 動的ルーティングを使用する際のディレクトリ名の変更(#19)

--- src/app/Chat/[sessionId]/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { DynamicChat } from '@/Components/parts/Chat/DynamincChat';
-const ChatPage: React.FC = () => {
-  return (
-    <DynamicChat />
-  );
-};
-export default ChatPage;
