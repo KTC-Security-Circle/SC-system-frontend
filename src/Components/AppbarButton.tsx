3474c2df30c6241535f381ca1550373e458de109
hourensou26 <ktc23a31g0002@edu.kyoto-tech.ac.jp>
2025-01-06 20:16:13 +0900

    ボタンを一つにまとめる

--- src/Components/AppbarButton.tsx
+++ src/Components/AppbarButton.tsx
@@ -1,63 +1,16 @@
 "use client";
 
 import * as React from "react";
-import { styled, alpha } from "@mui/material/styles";
 import {
   IconButton,
   Menu,
-  MenuProps,
   MenuItem,
   Divider,
   ListItemIcon,
   ListItemText,
 } from "@mui/material";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
-import EditIcon from "@mui/icons-material/Edit";
-import ArchiveIcon from "@mui/icons-material/Archive";
-import FileCopyIcon from "@mui/icons-material/FileCopy";
-import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import { AppbarButton } from "../types/appbar";
-
-// メニューのスタイルを定義
-const StyledMenu = styled((props: MenuProps) => (
-  <Menu
-    elevation={0}
-    anchorOrigin={{
-      vertical: "bottom",
-      horizontal: "right",
-    }}
-    transformOrigin={{
-      vertical: "top",
-      horizontal: "right",
-    }}
-    {...props}
-  />
-))(({ theme }) => ({
-  "& .MuiPaper-root": {
-    borderRadius: 6,
-    marginTop: theme.spacing(1),
-    minWidth: 180,
-    color: "#616161",
-    boxShadow:
-      "rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px",
-    "& .MuiMenu-list": {
-      padding: "4px 0",
-    },
-    "& .MuiMenuItem-root": {
-      "& .MuiSvgIcon-root": {
-        fontSize: 18,
-        color: theme.palette.text.secondary,
-        marginRight: theme.spacing(1.5),
-      },
-      "&:active": {
-        backgroundColor: alpha(
-          theme.palette.primary.main,
-          theme.palette.action.selectedOpacity
-        ),
-      },
-    },
-  },
-}));
 
 interface AppbarButtonProps {
   TextButtons: AppbarButton[]; // ボタンデータ配列
@@ -91,11 +44,34 @@ export const AppbarButtons: React.FC<AppbarButtonProps> = ({
         <MoreVertIcon />
       </IconButton>
 
-      {/* カスタムメニュー */}
-      <StyledMenu
+      {/* メニュー */}
+      <Menu
         anchorEl={anchorEl}
         open={isMenuOpen}
         onClose={handleMenuClose}
+        sx={{
+          "& .MuiPaper-root": {
+            borderRadius: 0, // 角を丸くしない
+            marginTop: 1,
+            minWidth: 180,
+            color: "#616161",
+            boxShadow:
+              "rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px",
+          },
+          "& .MuiMenu-list": {
+            padding: "4px 0",
+          },
+          "& .MuiMenuItem-root": {
+            "& .MuiSvgIcon-root": {
+              fontSize: 18,
+              color: "#616161", // グレーの色
+              marginRight: 1.5,
+            },
+            "&:active": {
+              backgroundColor: "#616161", // グレー背景（透明度 8%）
+            },
+          },
+        }}
       >
         {TextButtons.map((button, index) => (
           <React.Fragment key={index}>
@@ -109,11 +85,11 @@ export const AppbarButtons: React.FC<AppbarButtonProps> = ({
               <ListItemIcon>{button.icon}</ListItemIcon>
               <ListItemText>{button.text}</ListItemText>
             </MenuItem>
-            {/* Divider の挿入例 */}
+            {/* Divider の挿入 */}
             {index === 2 && <Divider sx={{ my: 0.5 }} />}
           </React.Fragment>
         ))}
-      </StyledMenu>
+      </Menu>
     </>
   );
 };
