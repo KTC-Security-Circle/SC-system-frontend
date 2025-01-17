202a3bc229915cc93abe47f9d18a7ea20b090bc2
Kitagawa19 <104189576+Kitagawa19@users.noreply.github.com>
2025-01-17 16:33:20 +0900

    Update AdminPageComponent.tsx

--- src/Components/AdminPageComponent.tsx
+++ src/Components/AdminPageComponent.tsx
@@ -4,7 +4,6 @@ import { Stack } from "@mui/system";
 import { ReactNode } from 'react';
 import AdminCard from "./parts/AdminCard";
 
-
 const ADMIN_PAGES = [
     {
         title: <CardTitle title={"学生管理ページ"} icon={<People />} />,
@@ -34,14 +33,17 @@ async function CardTitle({ title, icon }: { title: string, icon: ReactNode }) {
     )
 }
 
-export default async function AdminPageComponent() {
+export default  async function AdminPageComponent() {
     return (
         <Container>
-            <Stack sx={{ m: 5 }} spacing={2} direction={{ xs: "column", lg: "row" }}>
-                {ADMIN_PAGES.map((page) => {
-                    return <AdminCard {...page} />
-                })}
-            </Stack>
+            {ADMIN_PAGES.map((page, index) => (
+                <AdminCard
+                    key={index}
+                    title={page.title}
+                    description={page.description}
+                    href={page.href}
+                />
+            ))}
         </Container>
-    )
+    );
 }
