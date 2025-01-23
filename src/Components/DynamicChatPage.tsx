f492c1908781750385d63272cd919f5e0be1ca4d
Kitagawa19 <kkitagawa16219@gmail.com>
2025-01-23 21:47:42 +0900

    fix Containerの外側の色が違ったので修正(#38)

--- src/Components/DynamicChatPage.tsx
+++ src/Components/DynamicChatPage.tsx
@@ -216,6 +216,7 @@ export const DynamicChatwindow: React.FC<Props> = (props: Props) => {
             xs: '100%'
           },
           transition: 'width 0.3s ease-in-out',   
+          backgroundColor: '#e6ffff'
         }}
       >
         <DynamicChatComponent />
