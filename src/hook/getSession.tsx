2f0816ea324cc8b3312e02d7b68c251b217d716e
Kitagawa19 <kkitagawa16219@gmail.com>
2025-02-01 16:53:30 +0900

    add セッション名を取得する関数の作成(#43)

--- src/hook/getSession.tsx
+++ src/hook/getSession.tsx
@@ -3,17 +3,17 @@ import Cookies from 'js-cookie';
 
 const API_LINK = process.env.NEXT_PUBLIC_BACKEND_DEV_URL;
 
-export const fetchSessionItems = async()=>{
-    const token= Cookies.get('access_token');
-    if(!token){
+export const fetchSessionItems = async () => {
+    const token = Cookies.get('access_token');
+    if (!token) {
         throw new Error('No token found');
     }
-    const res = await axios.get(`${API_LINK}/api/view/session`,{
-        headers:{
+    const res = await axios.get(`${API_LINK}/api/view/session`, {
+        headers: {
             'Content-Type': 'application/json',
             'Authorization': `Bearer ${token}`,
             withCredentials: true, 
         },
     });
     return res.data;
-}
+};
