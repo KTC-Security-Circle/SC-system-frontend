5146478ec4811d41e6748d4faf4eba499145422c
hourensou26 <ktc23a31g0002@edu.kyoto-tech.ac.jp>
2025-02-05 21:39:02 +0900

    修正

--- src/Components/MarkdownNavgation.tsx
+++ src/Components/MarkdownNavgation.tsx
@@ -4,44 +4,49 @@ import { Stack } from "@mui/system";
 import { ReactNode } from 'react';
 import AdminCard from "./parts/AdminCard";
 
-
 const ADMIN_PAGES = [
     {
-        title: <CardTitle title={"学校情報の閲覧"} icon={<FindInPage />} />,
+        title: "学校情報の閲覧",
+        icon: <FindInPage />,
         description: "学校情報を閲覧します。",
         href: "/Information"
     },
     {
-        title: <CardTitle title={"学校情報の新規作成"} icon={<OpenInNew />} />,
+        title: "学校情報の新規作成",
+        icon: <OpenInNew />,
         description: "学校情報の新規作成をします。",
         href: "/Markdown"
     },
     {
-        title: <CardTitle title={"学校情報の編集"} icon={<BorderColor />} />,
+        title: "学校情報の編集",
+        icon: <BorderColor />,
         description: "学校情報を編集・削除します。",
         href: "/Markdown"
     }
-    ]
-    
-async function CardTitle({ title, icon }: { title: string, icon: ReactNode }) {
+];
+
+function CardTitle({ title, icon }: { title: string, icon: ReactNode }) {
     return (
         <Stack spacing={1} direction={"row"} alignItems={"center"} justifyItems={"center"}>
             {icon}
-            <div>
-                {title}
-            </div>
+            <div>{title}</div>
         </Stack>
-    )
+    );
 }
 
-export default async function AdminPageComponent() {
+export default function AdminPageComponent() {
     return (
         <Container>
             <Stack sx={{ m: 5 }} spacing={2} direction={{ xs: "column", lg: "row" }}>
-                {ADMIN_PAGES.map((page) => {
-                    return <AdminCard {...page} />
-                })}
+                {ADMIN_PAGES.map((page) => (
+                    <AdminCard
+                        key={page.href}
+                        title={<CardTitle title={page.title} icon={page.icon} />}
+                        description={page.description}
+                        href={page.href}
+                    />
+                ))}
             </Stack>
         </Container>
-    )
+    );
 }
