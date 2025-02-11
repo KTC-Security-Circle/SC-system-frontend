e9f5d68c46e46d98ee1c5644cd5761dc00b4dd08
Shieru-ktc <167487026+Shieru-ktc@users.noreply.github.com>
2025-02-11 15:35:24 +0900

    コンポーネントに分割した

--- src/app/LP/page.tsx
+++ src/app/LP/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
+import FeaturePart from '@/Components/parts/LP/FeaturePart';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import { Box, Button, Stack, Typography } from '@mui/material';
-import Grid from '@mui/material/Grid2';
 import { motion } from "framer-motion";
 import Image from 'next/image';
 import Link from 'next/link';
@@ -21,8 +21,8 @@ export default function LandingPage() {
         }
     }
 
-    return <>
-        <Box sx={{ textAlign: 'center', fontWeight: 'bold', margin: '1em', marginTop: '2.5em', display: 'flex', flexDirection: 'column', alignItems: 'center', fontSize: "1.5em" }}>
+    return <Box sx={{ backgroundColor: "#e6ffff", paddingTop: "2em" }}>
+        <Box sx={{ textAlign: 'center', fontWeight: 'bold', display: 'flex', flexDirection: 'column', alignItems: 'center', fontSize: "1.5em" }}>
             <Box sx={{ marginTop: '1em', justifyItems: "center", minHeight: "80vh" }}>
                 <Typography fontSize={"2em"} fontWeight={"bold"} style={{
                     textDecoration: "underline",
@@ -98,51 +98,132 @@ export default function LandingPage() {
                     ref={featuresRef}>
                     機能紹介
                 </Typography>
-                <Stack spacing={2}>
-                    <Grid container spacing={2} alignItems={"center"}>
-                        <Grid size={{ xs: 12, md: 6 }}>
-                            <Stack spacing={2} textAlign={{ xs: "center", md: "right" }}>
-                                <Typography fontSize={"1.2em"} fontWeight={"bold"}>チャットインターフェース</Typography>
-                                <Typography>
-                                    生成AIを活用した流れるような会話で、<br />
-                                    自然言語を使って簡単に指示できます。
-                                </Typography>
-                            </Stack>
-                        </Grid>
-                        <Grid size={6} alignItems={"center"} marginLeft={"auto"} marginRight={"auto"}>
-                            <Image src="https://placehold.co/350x240/png" width={350} height={240} alt="placeholder" />
-                        </Grid>
-                    </Grid>
+                <Stack spacing={8}>
+                    <FeaturePart
+                        title="チャットインターフェース"
+                        description={
+                            <>
+                                生成AIを活用した流れるような会話で<br />
+                                自然言語を使って簡単に指示できます
+                            </>
+                        }
+                        simulatedChatMessages={[
+                            {
+                                prompt: "何ができる？",
+                                response: (
+                                    <>
+                                        たくさんのことをお手伝いできます。
+                                        <br />
+                                        例えば、申請の作成、学校情報の検索、授業質問などにお答えできます。
+                                    </>
+                                )
+                            },
+                            {
+                                prompt: "今日ってなんの授業があったっけ？",
+                                response: (
+                                    <>
+                                        デッサン、数学、英語の授業があります。
+                                        <br />
+                                        本日のデザインの授業は休講です。
+                                    </>
+                                )
+                            },
+                            {
+                                prompt: "今日の夕飯何にしよう？",
+                                response: (
+                                    <>
+                                        今日の夕飯ですね。マクドナルドはいかがですか？ポテトが美味しいですよ。
+                                        <br />
+                                        京都デザイン&テクノロジー専門学校から、五条通を経由して徒歩15分です。
+                                        <br />
+                                        京都市営地下鉄「五条駅」の近くに位置しています。授業終わりに寄ってみてはいかがでしょう。
+                                    </>
+                                )
+                            }
+                        ]}
+                    />
 
-                    <Grid container spacing={2} alignItems={"center"} direction={{ xs: "column", md: "row-reverse" }}>
-                        <Grid size={{ xs: 12, md: 6 }}>
-                            <Stack spacing={2} textAlign={{ xs: "center", md: "left" }}>
-                                <Typography fontSize={"1.2em"} fontWeight={"bold"}>各種申請もお手伝い</Typography>
-                                <Typography>
-                                    申請内容を自動で判断。<br />
-                                    質問に答えるだけで、各種申請が完了します。
-                                </Typography>
-                            </Stack>
-                        </Grid>
-                        <Grid size={6} alignItems={"center"} marginLeft={"auto"} marginRight={"auto"}>
-                            <Image src="https://placehold.co/350x240/png" width={350} height={240} alt="placeholder" />
-                        </Grid>
-                    </Grid>
-
-                    <Grid container spacing={2} alignItems={"center"}>
-                        <Grid size={{ xs: 12, md: 6 }}>
-                            <Stack spacing={2} textAlign={{ xs: "center", md: "right" }}>
-                                <Typography fontSize={"1.2em"} fontWeight={"bold"}>学校情報の検索</Typography>
-                                <Typography>
-                                    わからないことはAIに聞いてみましょう。<br />
-                                    学校情報を検索し、情報をもとに回答します。
-                                </Typography>
-                            </Stack>
-                        </Grid>
-                        <Grid size={6} alignItems={"center"} marginLeft={"auto"} marginRight={"auto"}>
-                            <Image src="https://placehold.co/350x240/png" width={350} height={240} alt="placeholder" />
-                        </Grid>
-                    </Grid>
+                    <FeaturePart
+                        title="各種申請もお手伝い"
+                        description={
+                            <>
+                                申請内容を自動で判断<br />
+                                質問に答えるだけで、各種申請が完了します
+                            </>
+                        }
+                        reversed
+                        simulatedChatMessages={[
+                            {
+                                prompt: "電車が遅延したから公欠申請を出したい",
+                                response: (
+                                    <>
+                                        はい、公欠申請についてのお手伝いですね。
+                                        <br />
+                                        どの授業の公欠申請を出しますか？
+                                    </>
+                                )
+                            },
+                            {
+                                prompt: "1限にあるデザインの授業",
+                                response: (
+                                    <>
+                                        かしこまりました。1限目、佐藤先生の「デザイン概論」について、
+                                        <br />
+                                        公共交通機関の遅延を理由に公欠を申請します。
+                                        <br />
+                                        よろしいですか？
+                                    </>
+                                )
+                            },
+                            {
+                                prompt: "2限のJavaの授業も行けないから公欠出してほしい",
+                                response: (
+                                    <>
+                                        かしこまりました。以下の授業について、公共交通機関の遅延を理由に公欠を申請します。
+                                        <br />
+                                        1. 1限目、佐藤先生の「デザイン概論」
+                                        <br />
+                                        2. 2限目、田中先生の「Javaプログラミング」
+                                        <br />
+                                        よろしいですか？
+                                    </>
+                                )
+                            }
+                        ]}
+                    />
+                    <FeaturePart
+                        title="学校情報の検索"
+                        description={
+                            <>
+                                わからないことはAIに聞いてみましょう<br />
+                                学校情報を検索し、情報をもとに回答します
+                            </>
+                        }
+                        simulatedChatMessages={[
+                            {
+                                prompt: "京都テックってどんな学校？",
+                                response: (
+                                    <>
+                                        京都デザイン&テクノロジー専門学校は、京都府京都市下京区に位置する専門学校です。
+                                        <br />
+                                        IT、ゲーム制作、esportsといった分野に特化しています。
+                                    </>
+                                )
+                            },
+                            {
+                                prompt: "どんな専攻があるの？",
+                                response: (
+                                    <>
+                                        UnityやUnreal Engineといったゲーム制作について学ぶ「ゲームクリエイター専攻」や、
+                                        <br />
+                                        PythonやWeb開発などITに関する知識を網羅的に学ぶ「ITプログラマー専攻」、
+                                        <br />
+                                        ゲームの戦略やマネジメントについて学ぶ「esportsプロマネジメント専攻」などがあります。
+                                    </>
+                                )
+                            }
+                        ]}
+                    />
                 </Stack>
             </Box>
 
@@ -154,5 +235,5 @@ export default function LandingPage() {
                 <Button variant="contained" color="primary" style={{ marginTop: '1.5em', width: "9em", height: "2.5em", fontSize: "1.05em" }}>使ってみる</Button>
             </Box>
         </Box>
-    </>
+    </Box>
 }
