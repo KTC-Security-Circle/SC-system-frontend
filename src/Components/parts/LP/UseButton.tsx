5b1ee5435ca5a49cd58fd5a0fab670d99b8e3202
Shieru-ktc <167487026+Shieru-ktc@users.noreply.github.com>
2025-02-11 15:46:24 +0900

    「使ってみる」ボタンに`/Home`へのリンクを配置

--- src/Components/parts/LP/UseButton.tsx
+++ src/Components/parts/LP/UseButton.tsx
@@ -1,5 +1,8 @@
-import { Button } from "@mui/material";
+"use client";
 
-export default async function UseButton() {
-    return <Button variant="contained" color="primary" style={{ marginTop: '1.5em', width: "9em", height: "2.5em", fontSize: "1.05em" }}>使ってみる</Button>
+import { Button } from "@mui/material";
+import Link from "next/link";
+
+export default function UseButton() {
+    return <Button variant="contained" color="primary" style={{ marginTop: '1.5em', width: "9em", height: "2.5em", fontSize: "1.05em" }} LinkComponent={Link} href="/Home">使ってみる</Button>
 }
