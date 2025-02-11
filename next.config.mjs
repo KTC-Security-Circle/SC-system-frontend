0052eafb09d0a7ba47b222f1ac534385a44274ff
Shieru-ktc <167487026+Shieru-ktc@users.noreply.github.com>
2025-02-11 15:54:44 +0900

    使わなくなったremotePatternsの設定を削除

--- next.config.mjs
+++ next.config.mjs
@@ -14,15 +14,6 @@ const nextConfig = {
 
     return config;
   },
-  images: {
-    remotePatterns: [
-      {
-        // TODO: プレースホルダー画像のAPIなので、使わなくなったら消す
-        protocol: "https",
-        hostname: "placehold.co",
-      },
-    ],
-  },
 };
 
 export default nextConfig;
