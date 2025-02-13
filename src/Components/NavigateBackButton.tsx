cf0a7c34b8200ad503b91d13bda5a09fbd53ce22
hourensou26 <ktc23a31g0002@edu.kyoto-tech.ac.jp>
2025-02-13 16:30:19 +0900

    hrefの変更

--- src/Components/NavigateBackButton.tsx
+++ src/Components/NavigateBackButton.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BackButton } from '../types/navigateback';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
+import { Box, Button, Stack } from '@mui/material';
 
 interface BackButtonProps {
   TextButtons: BackButton[]; // ボタンデータ配列
@@ -10,20 +9,22 @@ interface BackButtonProps {
 
 export const NavigateBackButton:React.FC<BackButtonProps> =({TextButtons}) =>{
     return (
-      <Stack direction="row" spacing={2}>
-        {TextButtons.map((button: BackButton, index) => (
-          <Button
-            key={index}
-            variant="contained" 
-            color={button.color as 'inherit' | 'primary' | 'secondary' | 'success' | 'error' | 'info' | 'warning'} 
-            startIcon={button.icon}
-            sx={{
-                color: button.color || "#616161" // デフォルトで黒色
-            }}
-            href={button.href}>
-            {button.text}
-        </Button>
-        ))}
-      </Stack>
+      <Box sx={{ m: 5 }}>
+        <Stack direction="row" spacing={2}>
+          {TextButtons.map((button: BackButton, index) => (
+            <Button
+              key={index}
+              variant="contained" 
+              color={button.color as 'inherit' | 'primary' | 'secondary' | 'success' | 'error' | 'info' | 'warning'} 
+              startIcon={button.icon}
+              sx={{
+                  color: button.color || "#616161" // デフォルトで黒色
+              }}
+              href={button.href}>
+              {button.text}
+          </Button>
+          ))}
+        </Stack>
+      </Box>
     );
   };
--- src/app/Admin/School/page.tsx
+++ src/app/Admin/School/page.tsx
@@ -2,7 +2,6 @@ import MarkdownNavigation from "@/Components/MarkdownNavgation";
 import { BackButton } from "@/types/navigateback";
 import { NavigateBackButton } from "@/Components/NavigateBackButton";
 import { Container } from "@mui/material";
-import { Box } from "@mui/system";
 import { KeyboardReturn } from '@mui/icons-material';
 
 const TextButtons: BackButton [] = [
@@ -12,10 +11,8 @@ const TextButtons: BackButton [] = [
 export default async function AdminSchoolPage() {
     return (
         <Container>
-            <Box sx={{ m: 5 }}>
                 <NavigateBackButton TextButtons={TextButtons} />
                 <MarkdownNavigation />
-            </Box>
         </Container>
     )
 }
